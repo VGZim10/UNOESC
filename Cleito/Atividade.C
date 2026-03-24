c4daa6339ca8d9e2ff9c75fd3c47dfe7556e9685
Argonauta <victorguilhermedarosa212907@gmail.com>
2026-03-24 20:53:22 -0300

    piazinho

--- Cleito/Atividade.C
+++ Cleito/Atividade.C
@@ -5,12 +5,16 @@ input[9:0] SW
 );
 
 // assing LEDR[0] = KEY[0]& ~SW[0] | KEY[1] & SW[0]
-
-always @*
+reg z;
+always @*  //SENCIVEL A QUALQUER ENTRADA E A UM TIPO DE VARIAVEL ESPICÍFICA TAMBÉM (REG)
+beguin    
     case(SW{0})
     
-    1'b0 :
-    1'b1 :
-    
+    1'b0 : z <= KEY[0]
+    1'b1 : z <= KEY[1]
+
     endcase
+end
+assing LEDR[0] = z;
+
 endmodule
